50f900e75b805ec595f582521e9f1f8b10282b42
dennisgarrn <Dennis@webit.home>
2015-03-17 11:13:05 +0100

    "

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/views/pages'));
 
 // use res.render to load up an ejs view file
 
