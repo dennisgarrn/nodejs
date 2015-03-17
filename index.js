9ad64a94bf78e777dcfca95e8c06991e7f7586f8
dennisgarrn <Dennis@webit.home>
2015-03-17 11:09:14 +0100

    5 commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // server.js
 // load the things we need
-var express =require('express');
+var express = require('express');
 var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));
 
 // use res.render to load up an ejs view file
 
