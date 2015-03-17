9fdfbe1c01b78bc5fb1fc21148df97cf7056b2e7
dennisgarrn <Dennis@webit.home>
2015-03-17 11:37:39 +0100

    Finally it works

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-var express = require('express');
+// load the things we need
+var express =require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+// index page
 app.get('/', function(req, res){
 	res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res){
+	res.render('pages/about');
 });
 
 app.listen(app.get('port'), function() {
