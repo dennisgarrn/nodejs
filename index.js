531b3968beb69d1c43883bd1b92009cdb9ee3238
dennisgarrn <Dennis@webit.home>
2015-03-17 11:31:07 +0100

    "

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res){
+	res.render('pages/index');
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
